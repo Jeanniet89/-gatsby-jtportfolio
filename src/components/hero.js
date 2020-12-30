6abdf7132ecfa16d43738f37f987fdb85da991ec
Jeannie Torres <jeanniet89@gmail.com>
2020-12-30 03:19:28 -0500

    Hero GraphQl queries added

--- src/components/hero.js
+++ src/components/hero.js
@@ -13,19 +13,23 @@ const StyledSection = styled.section`
   }
 `
 
-const Hero = () => {
+const Hero = ({content}) => {
+    const { frontmatter, rawMarkdownBody } = content
     return (
         <StyledSection id="hero">
             <h1 className="title">
-                Hello <span role="img" aria-label="emoji">👋</span><br />
-        I'm Jeannie Torres
-      </h1>
+                {frontmatter.greetings}{" "}
+                <span role="img" aria-label="emoji">
+                    {frontmatter.emoji}
+                </span>
+                <br />
+                {frontmatter.title}
+            </h1>
             <h2 className="subtitle">
-                I like to build <span className="highlighted">things for the web</span>.
-      </h2>
-            <div className="description">
-                Full Stack Web Developer. Based out of Miami.
-      </div>
+                {frontmatter.subtitlePrefix}{" "}
+                <span className="highlighted">{frontmatter.subtitleHighlight}</span>
+            </h2>
+            <div className="description">{rawMarkdownBody}</div>
         </StyledSection>
     )
 }
